9e274e9535f9e96dabaacd3c9bbcc7cc22d25cbe
Young Han <yhan.ccs@gmail.com>
2014-11-26 16:59:10 -0800

    Adds Pager class

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,19 @@
 <?php
 //config.php
 
-include 'credentials.php';
+include 'credentials.php'; #database credentials
+include 'common.php'; #common functions
 
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 define('DEBUG',TRUE); #we want to see all errors
 
 date_default_timezone_set('America/Los_Angeles'); #sets default date/timezone for this website
+
+/* automatic path settings - use the following 4 path settings for placing all code in one application folder */ 
+define('VIRTUAL_PATH', 'http://www.younghan.org/itc240/fl14/'); # Virtual (web) 'root' of application for images, JS & CSS files
+define('PHYSICAL_PATH', '/home/youhan3/younghan.org/itc240/fl14/'); # Physical (PHP) 'root' of application for file & upload reference
+define('INCLUDE_PATH', PHYSICAL_PATH . 'includes/'); # Path to PHP include files - INSIDE APPLICATION ROOT
+
 
 # End Config area --------------------------------
 ob_start();  #buffers our page to be prevent header errors. Call before INC files or ANY html!
@@ -85,92 +92,3 @@ switch(THIS_PAGE)
 		$image = 'index.jpg';
 		$pageH1 = 'Global Warming';
 	}
-
-function makeLinks($nav)
-{
-	$myReturn = '';
-	foreach($nav AS $url => $label)
-	{
-		if($url == THIS_PAGE)
-		{//current page, add class
-		$myReturn .= '<li class="current"><a href="' . $url . '">' . $label . '</a></li>';
-		}
-		else
-		{//no class
-		$myReturn .= '<li><a href="' . $url . '">' . $label . '</a></li>';	
-		}
-	}
-	return $myReturn;
-}
-
-/*
-#builds and sends a safe email, using Reply-To properly!
-$today = date("Y-m-d H:i:s");
-$to = 'yhan0010@seattlecentral.edu';
-$replyTo = 'yhan.ccs@gmail.com';
-$subject = 'Test Email, includes ReplyTo: ' . $today;
-$message = 'Test Message Here.';
-
-safeEmail($to, $subject, $message, $replyTo='');
-*/
-
-function safeEmail($to, $subject, $message, $replyTo='')
-{#builds and sends a safe email, using Reply-To properly!
-$fromDomain = $_SERVER["SERVER_NAME"];
-$fromAddress = "noreply@" . $fromDomain; //form always submits from domain where form resides
-if($replyTo==''){$replyTo='';}
-$headers = 'From: ' . $fromAddress . PHP_EOL .
-'Reply-To: ' . $replyTo . PHP_EOL .
-'X-Mailer: PHP/' . phpversion();
-return mail($to, $subject, $message, $headers);
-}
-
-function process_post()
-{//loop through POST vars and return a single string
-    $myReturn = ''; //set to initial empty value
-
-    foreach($_POST as $varName=> $value)
-    {#loop POST vars to create JS array on the current page - include email
-         $strippedVarName = str_replace("_"," ",$varName);#remove underscores
-        if(is_array($_POST[$varName]))
-         {#checkboxes are arrays, and we need to collapse the array to comma separated string!
-             $myReturn .= $strippedVarName . ": " . implode(",",$_POST[$varName]) . PHP_EOL;
-         }else{//not an array, create line
-             $myReturn .= $strippedVarName . ": " . $value . PHP_EOL;
-         }
-    }
-    return $myReturn;
-} 
-
-function myerror($myFile, $myLine, $errorMsg)
-{
-    if(defined('DEBUG') && DEBUG)
-    {
-       echo "Error in file: <b>" . $myFile . "</b> on line: <b>" . $myLine . "</b><br />";
-       echo "Error Message: <b>" . $errorMsg . "</b><br />";
-       die();
-    }else{
-        echo "I'm sorry, we have encountered an error.  Would you like to buy some socks?";
-        die();
-    }
-}
- 
-/**
- * Wrapper function for processing data pulled from db
- *
- * Forward slashes are added to MySQL data upon entry to prevent SQL errors.  
- * Using our dbOut() function allows us to encapsulate the most common functions for removing  
- * slashes with the PHP stripslashes() function, plus the trim() function to remove spaces.
- *
- * Later, we can add to this function sitewide, as new requirements or vulnerabilities develop.
- *
- * @param string $str data as pulled from MySQL
- * @return $str data cleaned of slashes, spaces around string, etc.
- * @see dbIn()
- * @todo none
- */
-function dbOut($str)
-{
-	if($str!=""){$str = stripslashes(trim($str));}//strip out slashes entered for SQL safety
-	return $str;
-} #End dbOut()
